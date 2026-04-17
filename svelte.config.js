d3732392132062f7af983d26571d1c6ae325a71c
Chef Tech <cheftech@patraldo.com>
2026-04-17 16:43:27 -0600

    Fix svelte.config.js: remove invalid  import

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import { dev } from '$app/environment';
-import { adapter } from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter({
-            platformProxy: {
-                enabled: dev ? true : undefined
-            }
-        })
+        adapter: adapter()
     }
 };
 
